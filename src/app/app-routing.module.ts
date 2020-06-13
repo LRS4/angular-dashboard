61aef67355d94a18dc59c3d981e7aedfab887dfa
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-06-13 18:14:13 +0100

    Setup main components and router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
+import { SectionSalesComponent } from '../app/sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from '../app/sections/section-orders/section-orders.component';
+import { SectionHealthComponent } from '../app/sections/section-health/section-health.component';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sales', component: SectionSalesComponent },
+  { path: 'orders', component: SectionOrdersComponent },
+  { path: 'health', component: SectionHealthComponent },
+  { path: '', redirectTo: '/sales', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
