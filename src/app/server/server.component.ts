2911fe478293c863fe88759fb9dd7a4555013e28
LRS4 <lrspencer@hotmail.co.uk>
2020-06-14 18:53:09 +0100

    Update server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,13 +10,30 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  public color: string;
+  public buttonText: string;
+
   @Input() serverInput: Server;
 
   ngOnInit(): void {
+    this.setServerStatus(this.serverInput.isOnline)
+  }
+
+  setServerStatus(isOnline: boolean): void {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
   }
 
   toggleStatus(onlineStatus: boolean): void {
     console.log(this.serverInput.name, ':', onlineStatus);
+    this.setServerStatus(!onlineStatus);
   }
 
 }
