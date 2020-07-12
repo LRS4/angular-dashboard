fa1d9d5576fa5437e3d4dd7c54f162c082885c46
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-07-12 14:16:25 +0100

    Connect pie charts to sales data service and theme

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { __values } from 'tslib';
+import { THEME_COLORS } from '../../shared/theme.colors';
+var _ = require('lodash');
+
+const theme = 'Bright';
 
 @Component({
   selector: 'app-pie-chart',
@@ -9,17 +14,35 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
-  public pieChartData: number[] = [350, 450, 120];
-  public pieChartLabels: string[] = ['Facebook', 'Netflix', 'Google'];
+  @Input() inputData: any;
+  @Input() limit: number;
+
+  public pieChartData: number[];
+  public pieChartLabels: string[];
+
   public colors: any[] = [
     {
-      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      backgroundColor: this.themeColors(theme),
       borderColor: '#111'
     }
   ];
+
   public pieChartType: string = 'doughnut';
 
   ngOnInit(): void {
+    this.parseChartData(this.inputData, this.limit);
+  }
+
+  parseChartData(res: any, limit?: number) {
+    const allData = res.slice(0, limit);
+    this.pieChartData = allData.map(x =>  _.values(x)[1]);
+    this.pieChartLabels = allData.map(x => _.values(x)[0]);
+  }
+
+  themeColors(setName:string): string[] {
+    const colors = THEME_COLORS.slice(0)
+      .find(set => set.name === setName).colorSet;
+    return colors;
   }
 
 }
