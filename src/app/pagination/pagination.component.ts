2bf7e5aa6e8a7cabe6516a23d79112426432ee1d
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-07-12 12:28:10 +0100

    Finish orders pagination component

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -41,4 +41,49 @@ export class PaginationComponent implements OnInit {
   isLastPage(): boolean {
     return this.perPage * this.page >= this.count;
   }
+
+  getMin(): number {
+    return ((this.perPage * this.page) - this.perPage) + 1;  
+  }
+
+  getMax(): number {
+    let max = this.perPage * this.page;
+    if (max > this.count) {
+      max = this.count;
+    }
+    return max;
+  }
+
+  getPages(): number[] {
+    const totalPages = Math.ceil(this.count / this.perPage); // Say 10 pages
+    const thisPage = this.page || 1; // Say page 1
+    const pagesToShow = this.pagesToShow || 9; // Say max 10 pages to show
+    const pages: number[] = []; // Setup new empty array
+    pages.push(thisPage); // Push page 1 into array
+
+    console.log("Starting loop with: total pages: ", totalPages, "thisPage: ", thisPage, "pagesToShow: ", pagesToShow);
+    // Loop from zero to length of pages to show
+    for (let i = 0; i < pagesToShow - 1; i++) {
+      console.log("pages[]: ", pages);
+
+      if (pages.length < pagesToShow) {
+        // Check minimum value of pages array is greater than 1
+        if (Math.min.apply(null, pages) > 1) {
+          pages.push(Math.min.apply(null, pages) - 1);
+          console.log("Pushing ", Math.min.apply(null, pages) - 1, "onto array");
+        }
+      }
+
+      if (pages.length < pagesToShow) {
+        // Check maxmimum value of pages array is less than total pages
+        if (Math.max.apply(null, pages) < totalPages) {
+          pages.push(Math.max.apply(null, pages) + 1);
+          console.log("Pushing ", Math.max.apply(null, pages) + 1, "onto array")
+        }
+      }
+    }
+
+    pages.sort((a, b) => a - b);
+    return pages;
+  }
 }
