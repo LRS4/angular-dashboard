6bb86707c80ee50f291c63b1789cb6b59e8d5663
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-07-15 18:38:37 +0100

    Add types to sales data service http calls

--- src/app/services/sales-data.service.ts
+++ src/app/services/sales-data.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient as Http } from '@angular/common/http' ;
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class SalesDataService {
 
   constructor(private _http: Http) { }
 
-  getOrders(pageIndex: number, pageSize: number) {
-    return this._http.get('https://localhost:44364/api/order/' + pageIndex + '/' + pageSize)
+  getOrders<T>(pageIndex: number, pageSize: number): Observable<T[]> {
+    return this._http.get<T[]>('https://localhost:44364/api/order/' + pageIndex + '/' + pageSize)
       .map(res => res || []);
   }
 
-  getOrdersByCustomer(numberOfCustomers: number) {
-    return this._http.get('https://localhost:44364/api/order/bycustomer/' + numberOfCustomers)
+  getOrdersByCustomer<T>(numberOfCustomers: number): Observable<T[]> {
+    return this._http.get<T[]>('https://localhost:44364/api/order/bycustomer/' + numberOfCustomers)
       .map(res => res || []);
   }
 
-  getOrdersByState() {
-    return this._http.get('https://localhost:44364/api/order/bystate/')
+  getOrdersByState<T>(): Observable<T[]> {
+    return this._http.get<T[]>('https://localhost:44364/api/order/bystate/')
       .map(res => res || []);
   }
 }
