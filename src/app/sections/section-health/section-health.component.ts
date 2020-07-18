4841e355232e6628d6ff3517b8dc20ba19169679
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-07-18 14:49:20 +0100

    Implement update server action and response

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,25 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Server } from '../../shared/server';
+import { ServerService } from 'src/app/services/server.service';
+import { Observable } from 'rxjs/Rx';
+import { AnonymousSubscription } from 'rxjs/Subscription';
+import { ServerMessage } from 'src/app/shared/server-message';
 
-const SAMPLE_SERVERS: Server[] = [
-  { id: 1, name: 'dev-web', isOnline: true },
-  { id: 2, name: 'dev-mail', isOnline: false },
-  { id: 3, name: 'prod-web', isOnline: true },
-  { id: 4, name: 'prod-mail', isOnline: true },
-]
+// const SAMPLE_SERVERS: Server[] = [
+//   { id: 1, name: 'dev-web', isOnline: true },
+//   { id: 2, name: 'dev-mail', isOnline: false },
+//   { id: 3, name: 'prod-web', isOnline: true },
+//   { id: 4, name: 'prod-mail', isOnline: true },
+// ]
 
 @Component({
   selector: 'app-section-health',
   templateUrl: './section-health.component.html',
   styleUrls: ['./section-health.component.scss']
 })
-export class SectionHealthComponent implements OnInit {
+export class SectionHealthComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private _serverService: ServerService) { }
 
-  public servers: Server[] = SAMPLE_SERVERS;
+  public servers: Server[];
+  public timerSubscription: AnonymousSubscription;
 
   ngOnInit(): void {
+    this.refreshData();
   }
 
+  ngOnDestroy() {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  refreshData() {
+    this._serverService.getServers().subscribe(res => {
+      console.log(res);
+      this.servers = res;
+    });
+
+    this.subscribeToData();
+  }
+
+  subscribeToData() {
+    this.timerSubscription = Observable
+      .timer(5000)
+      .first()
+      .subscribe(() => this.refreshData());
+  }
+
+  sendMessage(message: ServerMessage) {
+    this._serverService.handleServerMessage(message)
+      .subscribe(res => console.log('Message sent to server:', message), err => console.log('Error', err));
+  }
 }
--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient as Http, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
+import { ServerMessage } from '../shared/server-message';
+import { Server } from '../shared/server';
+import 'rxjs/add/operator/catch';
+import { RequestOptions } from 'http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
 
-  constructor() { }
+  constructor(private _http: Http) { }
+
+  httpOptions: HttpHeaders
+
+  getServers(): Observable<Server[]> {
+    return this._http.get<Server[]>('https://localhost:44364/api/server')
+      .map(res => res || [])
+      .catch(this.handleError)
+  }
+
+  handleError(error: any) {
+    const message = (error.message)
+      ? error.message
+      : error.status
+        ? `${error.status} - ${error.statusText}`
+        : 'Server error';
+
+    console.log(message);
+    return Observable.throw(message);
+  }
+
+  handleServerMessage(message: ServerMessage): Observable<Response[]> {
+    const url = 'https://localhost:44364/api/server/' + message.id;
+    return this._http.put<Response[]>(url, message, {
+      'headers': new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'q=0.8;application/json;q=0.9'
+      })
+    }).map(res => res || [])
+  }
 }
