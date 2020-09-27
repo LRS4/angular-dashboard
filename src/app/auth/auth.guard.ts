e028b7a6ada18336f0aa988c3c22887632b25d62
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-09-27 18:20:49 +0100

    Add role authorisation to auth guard and intercept

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,17 +2,26 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private service: UserService) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
       if (localStorage.getItem('token') != null) {
+        let roles = next.data['permittedRoles'] as Array<string>;
+        if (roles) {
+          if (this.service.roleMatch(roles)) return true;
+          else {
+            this.router.navigate(['forbidden']);
+            return false;
+          }
+        }
         return true;
       } else {
         this.router.navigate(['user/login']);
